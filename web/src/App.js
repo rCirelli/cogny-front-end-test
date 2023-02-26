9dad36d731e670248efd4aee4c4779989654b92b
Raphael Cirelli <raphael.cirelli@protonmail.com>
2023-02-26 14:26:03 -0300

    feat: add components to App class, reset global styles

--- web/src/App.js
+++ web/src/App.js
@@ -1,9 +1,23 @@
+import Wrapper from './components/Wrapper';
 import Home from './pages/Home';
+import { createGlobalStyle } from 'styled-components'
 
 function App() {
+  const GlobalStyle = createGlobalStyle`
+    * {
+      box-sizing: border-box;
+      margin: 0;
+      padding: 0;
+      font-family: 'Roboto', sans-serif;
+    }
+  `
+
   return (
     <main>
-      <Home />
+      <GlobalStyle />
+      <Wrapper>
+        <Home />
+      </Wrapper>
     </main>
   );
 }
