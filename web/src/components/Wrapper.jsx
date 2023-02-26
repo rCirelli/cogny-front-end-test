3f2b262c73c4c057f35f04b9899fe90b7f85f985
Raphael Cirelli <raphael.cirelli@protonmail.com>
2023-02-26 19:58:14 -0300

    chore: optmize styling of Wrapper component

--- web/src/components/Wrapper.jsx
+++ web/src/components/Wrapper.jsx
@@ -1,15 +1,23 @@
 import styled from 'styled-components'
 
 const Container = styled.div`
-background-color: #191920;
-min-height: 100vh;
-padding: 50px 100px;
+  background-color: #191920;
+  min-height: 100vh;
+  padding: 50px 100px;
+  margin: auto;
+`
+
+const Wrap = styled.div`
+  max-width: 940px;
+  margin: auto;
 `
 
 function Wrapper({children}) {
   return (
     <Container>
-      {children}
+      <Wrap>
+        {children}
+      </Wrap>
     </Container>
   );
 }
