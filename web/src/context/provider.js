1f2156db9ca62772eea717b19e52a90c5699ee39
Raphael Cirelli <raphael.cirelli@protonmail.com>
2023-02-27 20:54:03 -0300

    refactor: unify 'cart' states on context

--- web/src/context/provider.js
+++ web/src/context/provider.js
@@ -1,39 +1,33 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import Context from './index';
 import useLocalStorage from '../hooks/useLocalStorage';
 
 function Provider({ children }) {
-  const [localCart, setLocalCart] = useLocalStorage('cart', []);
-  const [cartTotal, setCartTotal] = useLocalStorage('total', 0);
+  const [localCart, setLocalCart] = useLocalStorage('cart', { items: [], totalPrice: 0, totalQty: 0 });
 
-  function handleAddToCart(product) {
-    const currentCart = localCart;
-    const productIndex = currentCart.findIndex((item) => item.description === product.description);
-    if (productIndex !== -1) {
-      const newCart = structuredClone(localCart);
-      newCart[productIndex].qty += 1;
-      setLocalCart(newCart);
-    } else {
-      const newCart = structuredClone(currentCart);
-      newCart.push({...product, qty: 1});
-      setLocalCart(newCart);
-    }
+  function calculateCartTotals(cartItems) {
+    const totalPrice = Number(cartItems.reduce((acc, item) => acc + item.price * item.qty, 0).toFixed(2));
+    const totalQty = cartItems.reduce((acc, item) => acc + item.qty, 0);
+    return { totalPrice, totalQty }
   }
 
-  useEffect(() => {
-    function calculateCartTotal() {
-      const total = localCart.reduce((acc, item) => acc + item.price * item.qty, 0);
-      setCartTotal(total)
+  function handleAddToCart(product) {
+    const currentCart = structuredClone(localCart);
+    const productIndex = currentCart.items.findIndex((item) => item.description === product.description);
+
+    if (productIndex !== -1) {
+      currentCart.items[productIndex].qty += 1;
+    } else {
+      currentCart.items.push({...product, qty: 1});
     }
-    if (localCart.length > 0) {
-      calculateCartTotal(); 
-    }
-  }, [localCart, setCartTotal]);
+
+    const { totalPrice, totalQty } = calculateCartTotals(currentCart.items);
+    setLocalCart({ ...currentCart, totalPrice, totalQty });
+  }
 
   const contextValue = {
     localCart, setLocalCart,
-    cartTotal, setCartTotal,
     handleAddToCart,
   }
 
