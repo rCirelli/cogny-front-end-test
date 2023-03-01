69285e49385ce69a0189c1e7921c67b34767136d
Raphael Cirelli <raphael.cirelli@protonmail.com>
2023-03-01 17:24:15 -0300

    feat: define project name, structure, and base style

--- mobile/App.js
+++ mobile/App.js
@@ -1,21 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import Header from './src/components/Header.jsx';
+import Home from './src/Home'
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+  Roboto_900Black,
+} from '@expo-google-fonts/roboto';
+
+import Loading from './src/components/Loading.jsx';
 
 export default function App() {
+  const [fontsLoaded, err] = useFonts({
+    useFonts,
+    Roboto_400Regular,
+    Roboto_700Bold,
+    Roboto_900Black,
+  });
+
+  // if (!fontsLoaded) {
+  //   return <Loading />
+  // }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
+      <Header />
+      <Home />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    fontFamily: 'Roboto',
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#191920',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
   },
+
+  text: {
+    color: '#fff',
+  }
+
 });
